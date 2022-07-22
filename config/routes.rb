7eee7d4547ca6759ded7e58c2a1b70ad84a6e12f
Vb1221 <106549600+Vb1221@users.noreply.github.com>
2022-07-23 01:52:20 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :questions
+
+  # get '/questiond/:id/edit', to: 'questions#edit'
+
+  # post '/questions', to: 'questions#create'
+  
+  root 'pages#index'
 end
