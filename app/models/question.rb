70fd0a527fa0378d572364ae03684417ddf86076
Vb1221 <106549600+Vb1221@users.noreply.github.com>
2022-07-24 01:01:50 +0300

    add bootstrap

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
-  validates :title, presence: :true, length: {minimum: 2}
-  validates :body, presence: :true, length: {minimum: 5}
+
+  validates :title, presence: true, length: {minimum: 2}
+  validates :body, presence: true, length: {minimum: 2}
 
   def formated_created_at 
     created_at.strftime('%Y-%m-%d %H:%M')
